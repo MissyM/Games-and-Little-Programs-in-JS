86689f18fa53140f572816798ca715f7d6f794fe
Mile <mcabrerapatino@gmail.com>
2017-08-05 17:11:20 -0500

    Juego come galletas, version2

---
+++ comegalletas/juego.js
@@ -0,0 +1,22 @@
+var puntos = 0
+var segundos = 3
+function inicia() {
+	setTimeout( fin, segundos * 1000)
+}
+
+function posicion() {
+	var enemigo = document.getElementById("enemigo")
+	enemigo.style.position= "absolute"
+
+	x = Math.floor(Math.random()*500+1)
+	y = Math.floor(Math.random()*500+1)
+
+	enemigo.style.left = x + "px"
+	enemigo.style.top = y + "px" 
+
+puntos++
+
+}
+function fin() {
+	alert ("GAME OVER -Puntos:" + puntos)
+}
